61a1eb8d40190a2d83d4ee05b485673d8017eaf4
Albertus Liberius Humaniensis Disciplinus <w890702@gmail.com>
2018-05-08 18:26:47 +0900

    made v3 readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hamilton
 HAMilton is an extension of c++ std library
+
+[v3](v3/Hamliton/README.md)
---
+++ v3/Hamilton/Hamilton/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hamilton
+//
+//  Created by WOOHYUN RIM on 2018-05-08.
+//  Copyright © 2018 Protelum Liberium. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
