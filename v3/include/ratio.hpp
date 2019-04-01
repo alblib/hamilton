59c88014798a3cd80c2875b723a7532dfde37ecd
Albertus Liberius Humaniensis Disciplinus <w890702@gmail.com>
2019-04-01 16:38:25 +0900

    Update Style ratio.hpp

--- v3/include/ratio.hpp
+++ v3/include/ratio.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef __HAM_RATIO
+#define __HAM_RATIO 201904
 
 #include <ratio>
 
@@ -10,521 +11,523 @@
 
 namespace Hamilton {
 
-	namespace ratio {
+    namespace ratio {
 
-		using std::ratio;
+        using std::ratio;
 
-		using std::atto;
+        using std::atto;
 
-		using std::femto;
+        using std::femto;
 
-		using std::pico;
+        using std::pico;
 
-		using std::nano;
+        using std::nano;
 
-		using std::micro;
+        using std::micro;
 
-		using std::milli;
+        using std::milli;
 
-		using std::centi;
+        using std::centi;
 
-		using std::deci;
+        using std::deci;
 
-		using std::deca;
+        using std::deca;
 
-		using std::hecto;
+        using std::hecto;
 
-		using std::kilo;
+        using std::kilo;
 
-		using std::mega;
+        using std::mega;
 
-		using std::giga;
+        using std::giga;
 
-		using std::tera;
+        using std::tera;
 
-		using std::peta;
+        using std::peta;
 
-		using std::exa;
+        using std::exa;
 
 
 
-		template <class Ratio, class CharT>
+        template <class Ratio, class CharT>
 
-		struct ratio_string
+        struct ratio_string
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('[')) +
+                return std::basic_string<CharT>(1, CharT('[')) +
 
-					std::to_string(Ratio::num) + CharT('/') +
+                    std::to_string(Ratio::num) + CharT('/') +
 
-					std::to_string(Ratio::den) + CharT(']');
+                    std::to_string(Ratio::den) + CharT(']');
 
-			}
+            }
 
-		};
+        };
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::atto, CharT>
+        struct ratio_string<std::atto, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('a'));
+                return std::basic_string<CharT>(1, CharT('a'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::femto, CharT>
+        struct ratio_string<std::femto, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('f'));
+                return std::basic_string<CharT>(1, CharT('f'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::pico, CharT>
+        struct ratio_string<std::pico, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('p'));
+                return std::basic_string<CharT>(1, CharT('p'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::nano, CharT>
+        struct ratio_string<std::nano, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('n'));
+                return std::basic_string<CharT>(1, CharT('n'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <>
+        template <>
 
-		struct ratio_string<std::micro, char>
+        struct ratio_string<std::micro, char>
 
-		{
+        {
 
-			static std::string symbol() { return std::string("\xC2\xB5"); }
+            static std::string symbol() { return std::string("\xC2\xB5"); }
 
-		};
+        };
 
 
 
-		template <>
+        template <>
 
-		struct ratio_string<std::micro, char16_t>
+        struct ratio_string<std::micro, char16_t>
 
-		{
+        {
 
-			static std::u16string symbol() { return std::u16string(1, u'\xB5'); }
+            static std::u16string symbol() { return std::u16string(1, u'\xB5'); }
 
-		};
+        };
 
 
 
-		template <>
+        template <>
 
-		struct ratio_string<std::micro, char32_t>
+        struct ratio_string<std::micro, char32_t>
 
-		{
+        {
 
-			static std::u32string symbol() { return std::u32string(1, U'\xB5'); }
+            static std::u32string symbol() { return std::u32string(1, U'\xB5'); }
 
-		};
+        };
 
 
 
-		template <>
+        template <>
 
-		struct ratio_string<std::micro, wchar_t>
+        struct ratio_string<std::micro, wchar_t>
 
-		{
+        {
 
-			static std::wstring symbol() { return std::wstring(1, L'\xB5'); }
+            static std::wstring symbol() { return std::wstring(1, L'\xB5'); }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::milli, CharT>
+        struct ratio_string<std::milli, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('m'));
+                return std::basic_string<CharT>(1, CharT('m'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::centi, CharT>
+        struct ratio_string<std::centi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('c'));
+                return std::basic_string<CharT>(1, CharT('c'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::deci, CharT>
+        struct ratio_string<std::deci, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('d'));
+                return std::basic_string<CharT>(1, CharT('d'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<ratio<1, 1>, CharT>
+        struct ratio_string<ratio<1, 1>, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>();
+                return std::basic_string<CharT>();
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::deca, CharT>
+        struct ratio_string<std::deca, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('da'));
+                return std::basic_string<CharT>(1, CharT('da'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::hecto, CharT>
+        struct ratio_string<std::hecto, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('h'));
+                return std::basic_string<CharT>(1, CharT('h'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::kilo, CharT>
+        struct ratio_string<std::kilo, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('k'));
+                return std::basic_string<CharT>(1, CharT('k'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::mega, CharT>
+        struct ratio_string<std::mega, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('M'));
+                return std::basic_string<CharT>(1, CharT('M'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::giga, CharT>
+        struct ratio_string<std::giga, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('G'));
+                return std::basic_string<CharT>(1, CharT('G'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::tera, CharT>
+        struct ratio_string<std::tera, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('T'));
+                return std::basic_string<CharT>(1, CharT('T'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::peta, CharT>
+        struct ratio_string<std::peta, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('P'));
+                return std::basic_string<CharT>(1, CharT('P'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<std::exa, CharT>
+        struct ratio_string<std::exa, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>(1, CharT('E'));
+                return std::basic_string<CharT>(1, CharT('E'));
 
-			}
+            }
 
-		};
+        };
 
 
 
-		using kibi = ratio<1024, 1>;
+        using kibi = ratio<1024, 1>;
 
-		using mebi = ratio<1048576L, 1>;
+        using mebi = ratio<1048576L, 1>;
 
-		using gibi = ratio<1073741824L, 1>;
+        using gibi = ratio<1073741824L, 1>;
 
-		using tebi = ratio<1099511627776LL, 1>;
+        using tebi = ratio<1099511627776LL, 1>;
 
-		using pebi = ratio<1125899906842624LL, 1>;
+        using pebi = ratio<1125899906842624LL, 1>;
 
-		using exbi = ratio<1152921504606846976LL, 1>;
+        using exbi = ratio<1152921504606846976LL, 1>;
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<kibi, CharT>
+        struct ratio_string<kibi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('K'), CharT('i')};
+                return std::basic_string<CharT>{CharT('K'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<mebi, CharT>
+        struct ratio_string<mebi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('M'), CharT('i')};
+                return std::basic_string<CharT>{CharT('M'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<gibi, CharT>
+        struct ratio_string<gibi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('G'), CharT('i')};
+                return std::basic_string<CharT>{CharT('G'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<tebi, CharT>
+        struct ratio_string<tebi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('T'), CharT('i')};
+                return std::basic_string<CharT>{CharT('T'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<pebi, CharT>
+        struct ratio_string<pebi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('P'), CharT('i')};
+                return std::basic_string<CharT>{CharT('P'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
 
 
-		template <class CharT>
+        template <class CharT>
 
-		struct ratio_string<exbi, CharT>
+        struct ratio_string<exbi, CharT>
 
-		{
+        {
 
-			static std::basic_string<CharT> symbol()
+            static std::basic_string<CharT> symbol()
 
-			{
+            {
 
-				return std::basic_string<CharT>{CharT('E'), CharT('i')};
+                return std::basic_string<CharT>{CharT('E'), CharT('i')};
 
-			}
+            }
 
-		};
+        };
 
-	}
+    }
 
 }
+
+#endif
